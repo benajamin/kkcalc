9218d557fb3217ff1339dcc81230380e2cc0059b
Daniel Schick <schick.daniel@gmail.com>
2021-02-17 09:26:20 +0100

    update example with exp. Fe data

--- example/example.py
+++ example/example.py
@@ -12,21 +12,33 @@ import kkcalc as kk
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = ''  # full path and filename
-chemical_formular = ''
+filename = 'Fe.cf'  # full path and filename
+chemical_formula = 'Fe'
+
+x_min = 690
+x_max = 740
 
 output = kk.kk_calculate_real(filename,
-                              chemical_formular,
+                              chemical_formula,
                               load_options=None,
                               input_data_type='Beta',
-                              merge_points=None,
+                              merge_points=[x_min, x_max],
                               add_background=False,
                               fix_distortions=False,
                               curve_tolerance=None,
                               curve_recursion=50)
 
+Stoichiometry = kk.data.ParseChemicalFormula(chemical_formula)
+ASF_E, ASF_Data = kk.data.calculate_asf(Stoichiometry)
+ASF_Data2 = kk.data.coeffs_to_ASF(ASF_E, np.vstack((ASF_Data, ASF_Data[-1])))
+
 plt.figure()
-plt.plot(output[:, 0], output[:, 1], label='f1')
-plt.plot(output[:, 0], output[:, 2], label='f2')
+plt.plot(output[:, 0], output[:, 1], label='f1 kkcalc')
+plt.plot(output[:, 0], output[:, 2], label='f2 kkcalc')
+plt.plot(ASF_E, ASF_Data2, label='Henke f2')
 plt.legend()
+plt.xlim(x_min, x_max)
+plt.title('{:d} eV - {:d} eV'.format(x_min, x_max))
+plt.xlabel('Energy [eV]')
+plt.ylabel('f1, f2')
 plt.show()
